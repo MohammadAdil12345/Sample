0302acc62137e522eb67c26d1fcbf8d5145f8e85
MohammadAdil12345 <mohammadadil7921@gmail.com>
2023-10-20 17:24:06 +0530

    Test GIT

---
+++ src/org/tcs/Sample1.java
@@ -0,0 +1,14 @@
+package org.tcs;
+
+import java.util.Scanner;
+
+public class Sample1 {
+	public static void main(String[] args) {
+		float a=7/4f * 9/2f;
+		System.out.println(a);
+		
+		
+		
+	}
+
+}
